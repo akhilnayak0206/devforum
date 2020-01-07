0df546f3e3c561a37e19bb63ef974b55d6d25a1f
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-01-07 12:07:20 +0530

    alert redux

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 
 import './App.css';
 
@@ -20,8 +21,11 @@ const App = () => {
           <Navbar />
           <Route exact path='/' component={Landing} />
           <section className='container'>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
           </section>
         </Fragment>
       </Router>
